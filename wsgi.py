19354b3c95dbbb78aeee7ad8fd55dfa70f117e52
haiqiang-zhang <1552094108@qq.com>
2022-10-13 18:23:17 +1300

    https

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,11 @@
 """App entry point."""
 from MTMS import create_app
-import ssl
+from ssl import SSLContext, PROTOCOL_TLSv1_2
 
 app = create_app()
 
-context = ssl.SSLContext()
+context = SSLContext(PROTOCOL_TLSv1_2)
 context.load_cert_chain('cert.pem', 'key.pem')
 
 if __name__ == "__main__":
-    app.run(host='localhost', port=5000, ssl_context=context)
+    app.run(host='0.0.0.0', port=5000, ssl_context=context, debug=True)
