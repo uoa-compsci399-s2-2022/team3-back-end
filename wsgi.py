cbc906a53d0d7d4710834d8ed4101334c2a02d55
haiqiang-zhang <1552094108@qq.com>
2022-10-13 18:16:06 +1300

    https

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,11 @@
 """App entry point."""
 from MTMS import create_app
+import ssl
 
 app = create_app()
 
+context = ssl.SSLContext()
+context.load_cert_chain('cert.pem', 'key.pem')
+
 if __name__ == "__main__":
-    app.run(host='localhost', port=5000)
+    app.run(host='localhost', port=5000, ssl_context=context)
